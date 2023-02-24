2c9bf59a362f70e92dadeabd92d0a39ad9de97b3
KIM JUN SIK <silasoni94@gmail.com>
2023-02-24 22:07:17 +0900

    create: ecoprograms/admin.py 에서 Lay_Review class 추가

--- ecoprograms/admin.py
+++ ecoprograms/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from .models import Location, Ecoprogram, EcoprogramApply, Review
 
 
-admin.site.register(Location)
-admin.site.register(Ecoprogram)
-admin.site.register(Review)
-
-
 class Lay_EcoprogramApply(admin.ModelAdmin):
     list_display = ['ecoprogram', 'guest', 'result', 'created_at']
-
 admin.site.register(EcoprogramApply, Lay_EcoprogramApply)
+
+class Lay_Review(admin.ModelAdmin):
+    list_display = ['ecoprogram', 'user', 'content']
+admin.site.register(Review, Lay_Review)
+
+admin.site.register(Location)
+admin.site.register(Ecoprogram)
+
+
